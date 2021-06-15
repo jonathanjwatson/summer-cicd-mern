a390119dacda263c1703e9f11cc48662a504cb61
23carnies <46233990+23carnies@users.noreply.github.com>
2021-06-15 19:42:19 -0400

    Add server running console.log

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(express.json());
 
 app.use(express.static("client/build"));
 
+console.log('the server is running')
+
 mongoose
   .connect(process.env.MONGODB_URI || "mongodb://localhost/summer-cicd-mern", {
     useNewUrlParser: true,
