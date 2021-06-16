2abbbdbba18d387eb661576618f7794aeaa6e027
Robert Parsons <rmpcoding@gmail.com>
2021-06-15 17:59:50 -0700

    Modifies Feature workflow

--- server.js
+++ server.js
@@ -1,44 +1,47 @@
-const express = require("express");
+const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-const routes = require("./routes");
+const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("client/build"));
+app.use(express.static('client/build'));
 
-console.log("Attempting to bypass GitHub Actions")
+console.log('Attempting to bypass GitHub Actions');
 
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost/summer-cicd-mern", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  })
-  .then(() => {
-    console.log("Successfully connected to MongoDB");
-  })
-  .catch((err) => {
-    console.log("Error connecting to MongoDB", err);
-  });
+    .connect(
+        process.env.MONGODB_URI || 'mongodb://localhost/summer-cicd-mern',
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true,
+        }
+    )
+    .then(() => {
+        console.log('Successfully connected to MongoDB');
+    })
+    .catch((err) => {
+        console.log('Error connecting to MongoDB', err);
+    });
 
-app.get("/api/config", (req, res) => {
-  res.json({
-    success: true,
-  });
+app.get('/api/config', (req, res) => {
+    res.json({
+        success: true,
+    });
 });
 
 app.use(routes);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
