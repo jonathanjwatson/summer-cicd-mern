002d0f1e2dc99e3edff23561e6925752cfdb3942
Modern Sapien <joncanales2@gmail.com>
2021-06-22 18:11:04 -0400

    removing console.log()

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ app.use(express.json());
 
 app.use(express.static("client/build"));
 
-console.log("I'm running")
-
 mongoose
   .connect(process.env.MONGODB_URI || "mongodb://localhost/summer-cicd-mern", {
     useNewUrlParser: true,
