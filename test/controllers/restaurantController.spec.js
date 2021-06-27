0df2c72523cc54650dc71822340fb5b228147648
Jonathan J. Watson <jonathanwatson1@gmail.com>
2021-06-27 13:17:51 -0400

    completed findAll

--- test/controllers/restaurantController.spec.js
+++ test/controllers/restaurantController.spec.js
@@ -10,8 +10,53 @@ chai.use(sinonChai);
 
 describe("restaurantController", () => {
   describe("findAll", () => {
-    it("should return the models if found", async () => {});
-    it("should return an error message if an error occurs", async () => {});
+    const sandbox = sinon.createSandbox();
+    afterEach(function () {
+      sinon.restore();
+      sandbox.restore();
+    });
+    const req = {
+      query: "banana",
+    };
+
+    const statusJsonSpy = sinon.spy();
+
+    const res = {
+      json: sinon.spy(),
+      status: sinon.stub().returns({ json: statusJsonSpy }),
+    };
+    it("should return the models if found", async () => {
+      const mockFindAll = {
+        find: function () {
+          return this;
+        },
+        sort: function () {
+          return Promise.resolve("success");
+        },
+      };
+
+      mongoose.Model.find = mockFindAll.find;
+      mongoose.Model.sort = mockFindAll.sort;
+      await restaurantController.findAll(req, res);
+      expect(res.json).to.have.been.calledWith("success");
+    });
+    it("should return an error message if an error occurs", async () => {
+      const mockFindAll = {
+        find: function () {
+          return this;
+        },
+        sort: function () {
+          return Promise.reject("error message");
+        },
+      };
+
+      mongoose.Model.find = mockFindAll.find;
+      mongoose.Model.sort = mockFindAll.sort;
+      await restaurantController.findAll(req, res);
+      await console.log("---");
+      expect(res.status).to.have.been.calledWith(422);
+      expect(statusJsonSpy).to.have.been.calledWith("error message");
+    });
   });
 
   describe("findById", () => {
