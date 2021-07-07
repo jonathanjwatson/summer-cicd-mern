1338f9224601ea2143898607ec2e3176e21e9a67
pohern <ohern_p@yahoo.com>
2021-07-07 12:53:56 -0500

    Update test

--- test/controllers/restaurantController.spec.js
+++ test/controllers/restaurantController.spec.js
@@ -1,30 +1,32 @@
+const mocha = require("mocha");
 const chai = require("chai");
 const mongoose = require("mongoose");
 const sinon = require("sinon");
 const sinonChai = require("sinon-chai");
-
 const restaurantController = require("../../controllers/restaurantController");
 
 const expect = chai.expect;
 chai.use(sinonChai);
 
+// describe("", () => {
+//     it("", () => {
+
+//     })
+// });
+const sandbox = sinon.createSandbox();
+afterEach(function () {
+  sinon.restore();
+  sandbox.restore();
+});
+const statusJsonSpy = sinon.spy();
+const req = { params: { id: 1 } };
+const res = {
+  json: sinon.spy(),
+  status: sinon.stub().returns({ json: statusJsonSpy }),
+};
+
 describe("restaurantController", () => {
   describe("findAll", () => {
-    const sandbox = sinon.createSandbox();
-    afterEach(function () {
-      sinon.restore();
-      sandbox.restore();
-    });
-    const req = {
-      query: "banana",
-    };
-
-    const statusJsonSpy = sinon.spy();
-
-    const res = {
-      json: sinon.spy(),
-      status: sinon.stub().returns({ json: statusJsonSpy }),
-    };
     it("should return the models if found", async () => {
       const mockFindAll = {
         find: function () {
@@ -58,72 +60,26 @@ describe("restaurantController", () => {
       expect(statusJsonSpy).to.have.been.calledWith("error message");
     });
   });
-
   describe("findById", () => {
-    const sandbox = sinon.createSandbox();
-    afterEach(function () {
-      sinon.restore();
-      sandbox.restore();
-    });
-
-    const req = {
-      params: {
-        id: 1,
-      },
-    };
-
-    const statusJsonSpy = sinon.spy();
-
-    const res = {
-      json: sinon.spy(),
-      status: sinon.stub().returns({ json: statusJsonSpy }),
-    };
-
-    it("should return a model if found", async () => {
-      // Arrange
+    it("Should return a model if found", async () => {
+      //Arrange, Act, Assert
       mongoose.Model.findById = sandbox
         .stub()
-        .returns(Promise.resolve("banana"));
-      // Act
+        .returns(Promise.resolve("new model"));
       await restaurantController.findById(req, res);
-      // Assert
-      // Is res.json called and passed the string from the Promise.resolve above.
-      expect(res.json).to.have.been.calledWith("banana");
+      expect(res.json).to.have.been.calledWith("new model");
     });
-    it("should return an error message if an error occurs", async () => {
-      // Arrange
+    it("Should return an error message if an error occurs", async () => {
       mongoose.Model.findById = sandbox
         .stub()
         .returns(Promise.reject("error message"));
-
-      // Act
       await restaurantController.findById(req, res);
-      await console.log("---");
-      // Assert
-      expect(res.status).to.have.been.calledWith(404);
+      await console.log("----");
+      expect(res.status).to.have.been.calledWith(422);
       expect(statusJsonSpy).to.have.been.calledWith("error message");
     });
   });
-
   describe("create", () => {
-    const sandbox = sinon.createSandbox();
-    afterEach(function () {
-      sinon.restore();
-      sandbox.restore();
-    });
-
-    const req = {
-      params: {
-        id: 1,
-      },
-    };
-
-    const statusJsonSpy = sinon.spy();
-
-    const res = {
-      json: sinon.spy(),
-      status: sinon.stub().returns({ json: statusJsonSpy }),
-    };
     it("should return the new model if created", async () => {
       mongoose.Model.create = sandbox
         .stub()
@@ -137,38 +93,17 @@ describe("restaurantController", () => {
         .returns(Promise.reject("error message"));
 
       await restaurantController.create(req, res);
-      await console.log("---");
-
+      await console.log("----");
       expect(res.status).to.have.been.calledWith(422);
       expect(statusJsonSpy).to.have.been.calledWith("error message");
     });
-  });
-
-  describe("update", () => {
-    it("should return the updated model", () => {});
-    it("should return an error message if an error occurs", async () => {});
-  });
-
-  describe("remove", () => {
-    const sandbox = sinon.createSandbox();
-    afterEach(function () {
-      sinon.restore();
-      sandbox.restore();
+    describe("update", () => {
+      it("should return the updated model", () => {});
+      it("should return an error message if an error occurs", async () => {});
     });
-
-    const req = {
-      params: {
-        id: 1,
-      },
-    };
-
-    const statusJsonSpy = sinon.spy();
+  });
+  describe("remove", () => {
     const dbRemove = sinon.spy();
-
-    const res = {
-      json: sinon.spy(),
-      status: sinon.stub().returns({ json: statusJsonSpy }),
-    };
     it("should findById and call the remove method", async () => {
       mongoose.Model.findById = sandbox
         .stub()
@@ -187,17 +122,15 @@ describe("restaurantController", () => {
 
       expect(res.json).to.have.been.called;
     });
-    // FIXME: Test failing
-    xit("should return an error message if an error occurs", async () => {
-      mongoose.Model.findById = sandbox
-        .stub()
-        .returns(Promise.reject("error message"));
+  });
+  xit("should return an error message if an error occurs", async () => {
+    mongoose.Model.findById = sandbox
+      .stub()
+      .returns(Promise.reject("error message"));
 
-      await restaurantController.remove(req, res);
-      await console.log("---");
-
-      expect(res.status).to.have.been.calledWith(422);
-      expect(statusJsonSpy).to.have.been.calledWith("error message");
-    });
+    await restaurantController.remove(req, res);
+    await console.log("----");
+    expect(res.status).to.have.been.calledWith(422);
+    expect(statusJsonSpy).to.have.been.calledWith("error message");
   });
 });
